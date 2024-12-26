77b3ff8c9332dc45cf7e962ed5fa609fe6692cf3
rab-dot <mask.93@internet.ru>
2024-12-26 16:11:22 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true,
+    },
+}
+
+
