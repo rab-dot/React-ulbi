b41a513a6f7403d4ed5f7906f3ff03f4781f4191
rab-dot <mask.93@internet.ru>
2024-12-27 09:13:49 +0300

    add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,28 @@ const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
       filename: '[name].[contenthash].js',
-      path: path.resolve(__dirname, 'build'),
+      path: path.resolve(__dirname, 'dist'),
       clean: true,
   },
   plugins: [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html') }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 }
 
 
