ee5c22bcc488960fc1ee1be84daefd7afd00ad75
rab-dot <rabdot3@gmail.com>
2025-10-22 11:09:43 +0300

    decomposition webpack config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { BuildOptions } from "./types/config";
+import webpack from "webpack";
+import path from "path";
+
+export function buildWebpackConfig(
+  options: BuildOptions
+): webpack.Configuration {
+  const { paths, mode } = options;
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoaders(),
+    },
+    resolve: buildResolvers(),
+  };
+}
