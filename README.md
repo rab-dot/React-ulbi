14b29c3ea67678106c641bd855916b2e876b7437
rab-dot <rabdot3@gmail.com>
2025-10-22 14:36:23 +0300

    config css modules

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.kufar.by/item/1037999727?block_name=main&r_block=all&rank=12&searchId=6fb0ee1f-69c1-4b3b-80aa-368470f914b1&total_rank=13
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Counter } from "./components/Counter";
+import "./index.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      1232131sdaaddadadsa
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
