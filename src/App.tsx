c94035bf47d57be3e92044104f068c5087167157
rab-dot <rabdot3@gmail.com>
2025-10-22 17:09:07 +0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
 import "./index.scss";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className="app">
-      1232131sdaaddadadsa
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback="loading">
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
       <Counter />
     </div>
   );
