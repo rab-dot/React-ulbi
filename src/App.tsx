94b37855095c7c2f0f273b851debf4dd922cd326
rab-dot <rabdot3@gmail.com>
2025-10-23 16:42:44 +0300

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
-import "./index.scss";
+import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense } from "react";
+import { Suspense, useContext } from "react";
+import { ThemeContext } from "./theme/ThemeContext";
+import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classNames/classNames";
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div
+      className={classNames("app", { hovered: true, selected: false }, [theme])}
+    >
+      <button onClick={() => toggleTheme()}>theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback="loading">
