f430c10ba4a01c4137b9d2b0f69ae0efbd7d5a9f
rab-dot <rabdot3@gmail.com>
2026-02-26 12:46:59 +0300

    added loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,25 +1,24 @@
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-import { routeConfig } from "../config/routeConfig";
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
+import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { PageLoader } from 'widgets/PageLoader/PageLoader';
+import { routeConfig } from '../config/routeConfig';
 
 const AppRouter = () => (
-    <Suspense fallback="loading">
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <div className="page-wrapper">{element}</div>
-                        </Suspense>
-                    }
-                />
-            ))}
-        </Routes>
-    </Suspense>
+    <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+            <Route
+                key={path}
+                path={path}
+                element={
+                    <Suspense fallback={<PageLoader />}>
+                        <div className="page-wrapper">{element}</div>
+                    </Suspense>
+                }
+            />
+        ))}
+    </Routes>
 );
 
 export default AppRouter;
